e2b67e0bba9f29e962fe9fd4a9b36cf5da6bf557
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 11:45:26 +0000

    Add 'bio' column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323113602) do
+ActiveRecord::Schema.define(version: 20150323114412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20150323113602) do
     t.string   "last_name"
     t.string   "username"
     t.boolean  "is_artist"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
