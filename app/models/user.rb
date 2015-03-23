cbe002dd73fc14966893fa598501ace1aaea34f8
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 12:20:32 +0000

    Add 'following' association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :active_relationships, 
-    class_name: "Relationship",
-    foreign_key: "follower_id",
-    dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
 end
