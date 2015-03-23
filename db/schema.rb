5aad966f8a7db024ba03e3fd3e3a7b9ac7545d0f
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 11:23:13 +0000

    Create 'Comments' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323110713) do
+ActiveRecord::Schema.define(version: 20150323111912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "artwork_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
