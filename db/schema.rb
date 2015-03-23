69f4efee3c6ccc35b72789dc2c1b44c2ceadd992
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 11:38:24 +0000

    Create 'Relationships' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323112613) do
+ActiveRecord::Schema.define(version: 20150323113602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20150323112613) do
   create_table "likes", force: :cascade do |t|
     t.integer  "artwork_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "artist_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
