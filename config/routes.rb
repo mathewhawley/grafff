330ce6ded939579b1c420d84e0a7ef23253f7983
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 18:58:20 +0000

    Add base tag to application.html.erb. Add corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'static#index'
 
   devise_for :users
+
+  get '/test/*path' => 'static#index'
+  get '/test/home' => 'static#index', as: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
