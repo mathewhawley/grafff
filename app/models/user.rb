535d171b6db1e8082ba585fbd51b4a4c639d3d2f
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 13:16:16 +0000

    Add user -> comments association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ActiveRecord::Base
 
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :comments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+user_1 = User.create(first_name: 'John', last_name: 'Smith', username: 'johnsmith', email: 'john.smith@gmail.com', password: 'password', bio: 'Lorem ipsum dolor sit amet', created_at: Time.now, last_sign_in_at: Time.now, is_artist: false)
