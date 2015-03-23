c1854073885352fe632199affe742dd08eba824c
Mathew Hawley <mathew@mathewhawley.com>
2015-03-23 11:08:07 +0000

    Add 'is_artist' column with value of 'boolean' to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323110324) do
+ActiveRecord::Schema.define(version: 20150323110713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150323110324) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
+    t.boolean  "is_artist"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
