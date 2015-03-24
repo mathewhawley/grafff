2153dc15f1b78cc1614ddf01436c83d8f7b94411
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 17:28:09 +0000

    Display artworks that a user has 'liked'

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,8 +1,14 @@
 app.controller('UserController', function($scope, $http, UserService){
 
   UserService.getCurrentUser().then(function(response) {
-    console.log(response.data);
+    
     $scope.currentUser = response.data;
+    $scope.currentUserArtworks = [];
+
+    angular.forEach($scope.currentUser.artworks, function(key, value) {
+      $scope.currentUserArtworks.push(key);
+    })
+
   });
 
 });
