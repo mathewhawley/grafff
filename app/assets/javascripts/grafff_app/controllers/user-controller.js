19020ce5c0a99cf68dcb5c743ded231b2adf367e
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 22:30:22 +0000

    Add basic structure for profile body. Add nav tabs for profile body

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,26 +1,20 @@
 app.controller('UserController', function($scope, $http, $routeParams, UserService){
 
   UserService.getAllUsers().then(function(response) {
-    
     $scope.allUsers = response.data;
-    
     angular.forEach($scope.allUsers, function(value, key) {
       if(value.id == $routeParams.id) {
         $scope.user = value;
       } 
     });
-
   });
 
   UserService.getCurrentUser().then(function(response) {
-    
     $scope.currentUser = response.data;
-    
     $scope.currentUserArtworks = [];
     angular.forEach($scope.currentUser.artworks, function(value, key) {
       $scope.currentUserArtworks.push(value);
     });
-
   });
 
 });
