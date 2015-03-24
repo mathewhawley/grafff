3ad2cffd2260ddf88cfd01247650fd12ac0b800b
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 20:31:58 +0000

    Create links from listed user name, to the profile view

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,13 +1,14 @@
-app.controller('UserController', function($scope, $http, UserService){
+app.controller('UserController', function($scope, $http, $routeProvider, UserService){
 
   UserService.getCurrentUser().then(function(response) {
     
     $scope.currentUser = response.data;
+    $scope.user = $scope.currentUser[params[$routeParams.id]];
+    
     $scope.currentUserArtworks = [];
-
-    angular.forEach($scope.currentUser.artworks, function(key, value) {
-      $scope.currentUserArtworks.push(key);
-    })
+    angular.forEach($scope.currentUser.artworks, function(value, key) {
+      $scope.currentUserArtworks.push(value);
+    });
 
   });
 
