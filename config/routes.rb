de7ba34ab35e5333c1928ba95b875b9bed011ff3
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 19:48:20 +0000

    Set Devise login as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'static#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   devise_for :users, :controllers => { registrations: 'registrations' }
 
