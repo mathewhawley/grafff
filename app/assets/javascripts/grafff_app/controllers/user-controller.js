c4063f5b63a9a49cb43bb6369393599c5b527880
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 13:40:18 +0000

    Display current user information on profile page

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,7 +1,7 @@
-app.controller('UserController', function($http, UserService){
+app.controller('UserController', function($scope, $http, UserService){
 
   UserService.getCurrentUser().then(function(response) {
-    console.log(response.data);
+    $scope.currentUser = response.data;
   });
 
 });
