c548d267bf7c2651d121d59c85e4563d394aa64b
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 18:39:55 +0000

    Add remaining user associations on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def get_current_user
-    @current_user = current_user.to_json(:include => [:comments, :artworks, :likes])
+    @current_user = current_user.to_json(:include => [:comments, :artworks, :likes, :followers, :following])
     render json: @current_user
   end
 
