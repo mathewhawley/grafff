2d7782e198b3119a977b64a5e2780da6d8f4c685
Mathew Hawley <mathew@mathewhawley.com>
2015-03-24 16:56:04 +0000

    Change variable naming in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def get_current_user
-    @user = current_user.to_json(:include => [:comments, :artworks, :likes])
-    render json: @user
+    @current_user = current_user.to_json(:include => [:comments, :artworks, :likes])
+    render json: @current_user
   end
 
 end
