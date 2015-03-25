eaf1c1b9cbb5c80794ba7e89475bc9f99e9ec866
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 17:09:09 +0000

    Get users liked artworks and display on page

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -12,14 +12,14 @@ app.controller('UserController', function($scope, $http, $routeParams, UserFacto
         $scope.user = value;
       };
     });
-
+    // console.log($scope.user.id);
     // User's 'likes'
     $scope.userLikes = $scope.user.likes;
 
     // Loop through likes, store ID in $scope.artworkIdArray
     $scope.artworkIdArray = [];
     angular.forEach($scope.userLikes, function(value, key) {
-      $scope.artworkIdArray.push(value.id);
+      $scope.artworkIdArray.push(value.artwork_id);
     });
 
   });
@@ -34,8 +34,31 @@ app.controller('UserController', function($scope, $http, $routeParams, UserFacto
 
 
   ArtworkFactory.getAllArtworks().then(function(response) {
+
     $scope.allArtworks = response.data;
-    console.log($scope.allArtworks);
+
+    function isLiked(artwork, artworkIdArray) {
+      for(var i = 0; i < artworkIdArray.length; i++) {
+        if(artwork.id === artworkIdArray[i]) {
+          return true;
+        }
+      }
+    }
+
+    function returnLikedArt(artworkIdArray, allArtworks) {
+      likedArt = [];
+      for(var i = 0; i < allArtworks.length; i++) {
+        if(isLiked(allArtworks[i], artworkIdArray)) {
+          likedArt.push(allArtworks[i]);
+        }
+      }
+        return likedArt;
+    }
+
+    $scope.userLikedArtwork = returnLikedArt($scope.artworkIdArray, $scope.allArtworks);
+
+    console.log($scope.userLikedArtwork);
+
   });
 
 });
