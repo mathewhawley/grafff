0ad21fb7291747a57abd5da84a1aae34cc913763
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 10:56:03 +0000

    Created profile controller. Move logic from view to controller

---
+++ app/assets/javascripts/grafff_app/controllers/profile-controller.js
@@ -0,0 +1,16 @@
+app.controller('ProfileController', function($scope) {
+
+  // Set initial tab
+  $scope.tab = 1;
+
+  // Select tab on click and set 'tab' value
+  $scope.selectTab = function(setTab) {
+    $scope.tab = setTab;
+  };
+
+  // Show content based on which tab is selected
+  $scope.isSelected = function(checkTab) {
+    return $scope.tab === checkTab;
+  };
+
+});
