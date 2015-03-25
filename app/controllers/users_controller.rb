f990a1ffb1db367042ffd5d3e00a849a37042a15
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 11:35:35 +0000

    Remove '@' from variables – redundant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
 
   def get_all_users
-    @all_users = User.all.to_json(:include => [:comments, :artworks, :likes, :followers, :following])
-    render json: @all_users
+    all_users = User.all.to_json(:include => [:comments, :artworks, :likes, :followers, :following])
+    render json: all_users
   end
 
   def get_current_user
-    @current_user = current_user.to_json(:include => [:comments, :artworks, :likes, :followers, :following])
-    render json: @current_user
+    logged_in_user = current_user.to_json(:include => [:comments, :artworks, :likes, :followers, :following])
+    render json: logged_in_user
   end
 
 end
