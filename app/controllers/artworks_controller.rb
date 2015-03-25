e28c1180720df9fe6577d458d006425a66b1a1e3
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 11:48:45 +0000

    Get all artworks and their associations from database, serve JSON

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,8 @@
 class ArtworksController < ApplicationController
   
   def get_all_artworks
-    
+    artworks = Artwork.all.to_json(:include => [:comments, :likes, :users])
+    render json: artworks
   end
 
 end
