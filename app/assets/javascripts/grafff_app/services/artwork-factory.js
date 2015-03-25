68d76201b3497b8e6b90e2f025a7040b8e60411b
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 11:31:43 +0000

    Create artwork factory

---
+++ app/assets/javascripts/grafff_app/services/artwork-factory.js
@@ -0,0 +1,9 @@
+app.factory('ArtworkFactory', function($http) {
+
+  var ArtworkFactory = {
+    getAllArtworks: function() {
+      return $http.get('/artworks/get_all_artworks');
+    }
+  }
+
+});
