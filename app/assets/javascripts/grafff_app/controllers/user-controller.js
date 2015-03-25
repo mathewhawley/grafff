b3d507fe5d162a73362b3286e7924716b69e9e39
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 11:28:47 +0000

    Get user's 'likes' and display each object on profile page

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,26 +1,33 @@
 app.controller('UserController', function($scope, $http, $routeParams, UserFactory){
 
   UserFactory.getAllUsers().then(function(response) {
+
     // Store all user data in '$scope.allUsers'
     $scope.allUsers = response.data;
     console.log($scope.allUsers);
+
     // Get the user that matches the id in params
     angular.forEach($scope.allUsers, function(value, key) {
       if(value.id == $routeParams.id) {
         $scope.user = value;
       };
     });
-    console.log($scope.user);
+
+    // User's 'likes'
+    $scope.userLikes = $scope.user.likes;
   });
 
   UserFactory.getCurrentUser().then(function(response) {
+
     // Store current user data in '$scope.currentUser'
     $scope.currentUser = response.data;
+
     // Store current user's associated artworks in '$scope.currentUserArtworks'
     $scope.currentUserArtworks = [];
     angular.forEach($scope.currentUser.artworks, function(value, key) {
       $scope.currentUserArtworks.push(value);
     });
+    
   });
 
 });
