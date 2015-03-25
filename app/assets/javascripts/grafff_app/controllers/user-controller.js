e9497a34197730b9b95acfd6e1cbd14c8e84f6f3
Mathew Hawley <mathew@mathewhawley.com>
2015-03-25 08:41:06 +0000

    Add comments to user-controller.js

--- app/assets/javascripts/grafff_app/controllers/user-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-controller.js
@@ -1,7 +1,9 @@
 app.controller('UserController', function($scope, $http, $routeParams, UserService){
 
   UserService.getAllUsers().then(function(response) {
+    // Store all user data in '$scope.allUsers'
     $scope.allUsers = response.data;
+    // Get the user that matches the id in params
     angular.forEach($scope.allUsers, function(value, key) {
       if(value.id == $routeParams.id) {
         $scope.user = value;
@@ -10,6 +12,7 @@ app.controller('UserController', function($scope, $http, $routeParams, UserServi
   });
 
   UserService.getCurrentUser().then(function(response) {
+    // Store current user data in '$scope.currentUser'
     $scope.currentUser = response.data;
     $scope.currentUserArtworks = [];
     angular.forEach($scope.currentUser.artworks, function(value, key) {
