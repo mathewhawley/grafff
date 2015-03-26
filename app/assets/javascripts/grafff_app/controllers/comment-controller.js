61a4730626c161bae5931f22ece5d07d1551e386
Mathew Hawley <mathew@mathewhawley.com>
2015-03-26 21:59:06 +0000

    Create comment object when 'enter' is pressed in the textarea

--- app/assets/javascripts/grafff_app/controllers/comment-controller.js
+++ app/assets/javascripts/grafff_app/controllers/comment-controller.js
@@ -1,5 +1,18 @@
-app.controller('CommentController', function($scope, $http) {
+app.controller('CommentController', function($scope, $http, UserFactory) {
 
   console.log('I am the comment controller');
+
+  UserFactory.getCurrentUser().then(function(response) {
+    $scope.currentUser = response.data;
+
+    $scope.postComment = function(comment) {
+      // Make a hash to be passed to database
+      var newComment = {};
+      newComment['content'] = comment.content;
+      newComment['user_id'] = $scope.currentUser.id;
+      newComment['artwork_id'] = $scope.artwork.id;
+      console.log(newComment);
+    };
+  });
 
 });
