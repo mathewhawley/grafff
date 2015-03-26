c0cd99a2cdc1fd3452d50fa72051fd8368ce0d60
Mathew Hawley <mathew@mathewhawley.com>
2015-03-26 21:01:53 +0000

    Include user association with comments made on artwork

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,7 @@
 class ArtworksController < ApplicationController
   
   def get_all_artworks
-    artworks = Artwork.all.to_json(include: [:comments, :likes, :users, :user])
+    artworks = Artwork.all.to_json(include: [{ comments: { include: :user }}, :likes, :users, :user])
     render json: artworks
   end
 
