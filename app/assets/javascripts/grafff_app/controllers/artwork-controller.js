300ddbda18f53657bb4cd9e5126041a9680cb317
Mathew Hawley <mathew@mathewhawley.com>
2015-03-26 20:52:42 +0000

    Create artwork route, get artwork from params

--- app/assets/javascripts/grafff_app/controllers/artwork-controller.js
+++ app/assets/javascripts/grafff_app/controllers/artwork-controller.js
@@ -1,8 +1,22 @@
-app.controller('ArtworkController', function($scope, $http, UserFactory, ArtworkFactory) {
+app.controller('ArtworkController', function($scope, $http, UserFactory, ArtworkFactory, $routeParams) {
 
   ArtworkFactory.getAllArtworks().then(function(response) {
 
+    // Store all user data in '$scope.allArtworks'
     $scope.allArtworks = response.data;
+
+    // Get artwork from params
+    function getArtworkFromParams(allArtworks, routeParams) {
+      for(var i = 0; i < allArtworks.length; i++) {
+        if(allArtworks[i].id === JSON.parse(routeParams.id)) {
+          var artwork = allArtworks[i];
+        };
+      };
+      return artwork;
+    }
+    $scope.artwork = getArtworkFromParams($scope.allArtworks, $routeParams);
+    console.log($scope.artwork);
+
 
     // Artwork Show Page
     
--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,7 @@
 class ArtworksController < ApplicationController
   
   def get_all_artworks
-    artworks = Artwork.all.to_json(include: [:comments, :likes, :users])
+    artworks = Artwork.all.to_json(include: [:comments, :likes, :users, :user])
     render json: artworks
   end
 
