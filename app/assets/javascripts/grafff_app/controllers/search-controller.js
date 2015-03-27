b2276aa75a6a8404e3dd1fd2e410369333e4259d
Mathew Hawley <mathew@mathewhawley.com>
2015-03-27 16:30:57 +0000

    Get search field input and log to the console

--- app/assets/javascripts/grafff_app/controllers/search-controller.js
+++ app/assets/javascripts/grafff_app/controllers/search-controller.js
@@ -1,5 +1,10 @@
-app.controller('SearchController', function() {
+app.controller('SearchController', function($scope, $http, UserFactory) {
 
-  
+  console.log('I am the Search Controller');
+
+  $scope.testFunction = function() {
+    console.log('working');
+    console.log($scope.query);
+  };
 
 });
