055a072d1d4f7eaa53731fb0519c5bc6c3141a5f
Mathew Hawley <mathew@mathewhawley.com>
2015-03-27 11:09:26 +0000

    Console.log a 'like' button click

--- app/assets/javascripts/grafff_app/controllers/like-controller.js
+++ app/assets/javascripts/grafff_app/controllers/like-controller.js
@@ -1,5 +1,14 @@
-app.controller('LikeController', function($scope, $http) {
+app.controller('LikeController', function($scope, $http, UserFactory, $routeParams) {
 
+  UserFactory.getCurrentUser().then(function(response) {
+    
+    $scope.currentUser = response.data;
+
+    $scope.likeArtwork = function() {
+      console.log('button clicked');
+    };
+    // Create 'like' object to send back to controller
+
+  });
   
-
 });
