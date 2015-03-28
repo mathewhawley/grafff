18cbb1321a0200c47e1e3e4368492dbcf6e07ac5
Mathew Hawley <mathew@mathewhawley.com>
2015-03-28 10:41:17 +0000

    Rename search function, console log tests

--- app/assets/javascripts/grafff_app/controllers/search-controller.js
+++ app/assets/javascripts/grafff_app/controllers/search-controller.js
@@ -2,9 +2,20 @@ app.controller('SearchController', function($scope, $http, UserFactory) {
 
   console.log('I am the Search Controller');
 
-  $scope.testFunction = function() {
-    console.log('working');
-    console.log($scope.query);
-  };
+  UserFactory.getAllUsers().then(function(response) {
+
+    function searchUsers(query) {
+      console.log('Searching for a match...');
+    }
+
+    $scope.searchUsers = function() {
+      console.log(response.data);
+      console.log($scope.query);
+      // Loop through all users
+      // If any part of username, first_name or last_name matches the query, console.log them thangs!
+      searchUsers($scope.query);
+    };
+
+  });
 
 });
