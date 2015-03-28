68c23472066696d12d336e19c759c86d80ea68c4
Mathew Hawley <mathew@mathewhawley.com>
2015-03-28 16:00:36 +0000

    Get nearest address to marker, display on view

--- app/assets/javascripts/grafff_app/controllers/map-controller.js
+++ app/assets/javascripts/grafff_app/controllers/map-controller.js
@@ -11,8 +11,14 @@ app.controller('MapController', function($scope, $http) {
   // });
 
   $scope.getPosition = function(position) {
-    $scope.longitude = position.latLng.D;
-    $scope.latitude = position.latLng.k;
+
+    var longitude = position.latLng.D;
+    var latitude = position.latLng.k;
+
+    $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true').then(function(response) {
+      $scope.nearestAddress = response.data.results[0].formatted_address;
+    });
+    
   };
 
 });
