53b154e8c61559dd6f5e8c622f9cccc917eba8fb
Mathew Hawley <mathew@mathewhawley.com>
2015-03-28 16:16:26 +0000

    Tidy map-controller.js and new-artwork-form.html

--- app/assets/javascripts/grafff_app/controllers/map-controller.js
+++ app/assets/javascripts/grafff_app/controllers/map-controller.js
@@ -1,15 +1,5 @@
 app.controller('MapController', function($scope, $http) {
 
-  // var map;
-  // $scope.$on('mapInitialized', function(evt, evtMap) {
-  //   map = evtMap;
-  //   $scope.placeMarker = function(e) {
-  //     var marker = new google.maps.Marker({position: e.latLng, draggable: true, map: map});
-  //     map.panTo(e.latLng);
-  //     console.log(marker.position);
-  //   };
-  // });
-
   $scope.getPosition = function(position) {
 
     var longitude = position.latLng.D;
@@ -18,7 +8,7 @@ app.controller('MapController', function($scope, $http) {
     $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true').then(function(response) {
       $scope.nearestAddress = response.data.results[0].formatted_address;
     });
-    
+
   };
 
 });
