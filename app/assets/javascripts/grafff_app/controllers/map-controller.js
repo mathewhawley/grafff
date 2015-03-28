f1872d1edf5d0c38294fb107f94a6d22eddd7990
Mathew Hawley <mathew@mathewhawley.com>
2015-03-28 15:07:31 +0000

    Attempt to get coordinates from draggable marker - logging coordinates on click, not on initial click and drag though...

--- app/assets/javascripts/grafff_app/controllers/map-controller.js
+++ app/assets/javascripts/grafff_app/controllers/map-controller.js
@@ -1,5 +1,18 @@
 app.controller('MapController', function($scope, $http) {
 
-  console.log('I am the Map Controller');
+  // var map;
+  // $scope.$on('mapInitialized', function(evt, evtMap) {
+  //   map = evtMap;
+  //   $scope.placeMarker = function(e) {
+  //     var marker = new google.maps.Marker({position: e.latLng, draggable: true, map: map});
+  //     map.panTo(e.latLng);
+  //     console.log(marker.position);
+  //   };
+  // });
+
+  $scope.test = function(position) {
+    console.log('marker clicked');
+    console.log(position);
+  };
 
 });
