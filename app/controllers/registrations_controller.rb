4335ac3def8b7b613d5979620c406548b9e80ae6
Mathew Hawley <mathew@mathewhawley.com>
2015-03-30 14:25:16 +0100

    Add 'profile pic' to registration strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :is_artist, :bio)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :is_artist, :bio, :profile_pic)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :is_artist, :bio)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :is_artist, :bio, :profile_pic)
   end
 
 end
