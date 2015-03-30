152f724520be2c0fd5b22172cc7e9f1f5adfea74
Mathew Hawley <mathew@mathewhawley.com>
2015-03-30 15:37:11 +0100

    Store any user that matches query into a 'search results' variable

--- app/assets/javascripts/grafff_app/controllers/search-controller.js
+++ app/assets/javascripts/grafff_app/controllers/search-controller.js
@@ -5,21 +5,20 @@ app.controller('SearchController', ['$scope','$http', 'UserFactory', function($s
     $scope.allUsers = response.data;
 
     $scope.searchUsers = function(query) {
-      // console.log($scope.allUsers);
       
       // Loop through all users
       // If any part of username, first_name or last_name matches the query, console.log them thangs!
       function filterAllUsers(allUsers, query) {
+        var searchResults = [];
         for(var i = 0; i< allUsers.length; i++) {
-          if(allUsers[i].first_name.indexOf(query) >= 0) {
-            console.log(allUsers[i]);
+          if(allUsers[i].first_name.indexOf(query) >= 0 || allUsers[i].last_name.indexOf(query) >= 0 || allUsers[i].username.indexOf(query) >= 0) {
+            searchResults.push(allUsers[i]);
           }
-          // console.log(allUsers[i].first_name);
-          // console.log(query);
         };
+        return searchResults;
       }
 
-      filterAllUsers($scope.allUsers, query);
+      $scope.searchResults = filterAllUsers($scope.allUsers, query);
   
     };
 
