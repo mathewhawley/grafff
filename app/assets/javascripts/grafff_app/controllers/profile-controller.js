afcd2a8943630be31be7792ffc30aa578ad0c82d
Mathew Hawley <mathew@mathewhawley.com>
2015-04-01 11:46:57 +0100

    Add 's' to http, profile-controller.js maps

--- app/assets/javascripts/grafff_app/controllers/profile-controller.js
+++ app/assets/javascripts/grafff_app/controllers/profile-controller.js
@@ -73,7 +73,7 @@ app.controller('ProfileController', ['$scope', '$http', '$route', 'ArtworkFactor
     longitude = position.latLng.D;
     latitude = position.latLng.k;
 
-    $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true').then(function(response) {
+    $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true').then(function(response) {
       $scope.nearestAddress = response.data.results[0].formatted_address;
     });
 
