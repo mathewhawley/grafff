b2a0fe02dc281962ef6288b328d677cbc1f02ce1
Mathew Hawley <mathew@mathewhawley.com>
2015-04-01 12:15:50 +0100

    Update config variables (AWS)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider  => 'AWS',  # required
-    :aws_access_key_id  => ENV['AWS_ACCESS_KEY_ID_WDI'],  # required
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_WDI'],  # required
+    :aws_access_key_id  => ENV['AWS_ACCESS_KEY_ID_APP_PUBLIC'],  # required
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_ID_APP_PUBLIC'],  # required
     :region  => 'eu-west-1',  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'grafff'  # required
