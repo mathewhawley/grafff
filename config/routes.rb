e9b7ccbd3cf5246f2783a678a2d81dc8a3b10307
Mathew Hawley <mathew@mathewhawley.com>
2015-04-09 12:08:54 +0100

    Re-organise routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,44 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  get '/users' => 'users#user_show'
-  get '/users/get_current_user' => 'users#get_current_user'
-  get '/users/get_all_users' => 'users#get_all_users'
-  get '/users/get_artists' => 'users#get_artists'
+  # Current user
+  get '/users/current_user' => 'users#get_current_user'
 
+  ##########################################################################################
+  # Getting content for templates
+  ##########################################################################################
+
+  # Homepage
+  get '/home/map' => 'artworks#main_map'
+  get '/home/popular_artists' => 'users#popular_artists'
+  get '/home/latest_artworks' => 'artworks#latest_artworks'
+
+  # Artwork index page
   get '/artworks' => 'artworks#index'
-  get '/artworks/get_latest_artworks' => 'artworks#get_latest_artworks'
-  get '/artworks/get_main_map_artworks' => 'artworks#get_main_map_artworks'
-  post '/artworks/add_artwork' => 'artworks#add_artwork'
 
-  get '/relationships/get_all_relationships' => 'relationships#get_all_relationships'
+  # User show page
+  get '/users/show' => 'users#show'
+  get '/users/likes' => 'likes#user_likes'
+  get '/users/followed_artists' => 'relationships#followed_artists'
+  get '/users/artworks' => 'artworks#user_artworks'
+
+  # Artist index page
+  get '/artists' => 'users#all_artists'
+
+
+  ##########################################################################################
+  # Functionality
+  ##########################################################################################
+
+  # User show page
+  post '/artworks/new_artwork' => 'artworks#new_artwork'
   post '/relationships/follow_user' => 'relationships#follow_user'
   post '/relationships/unfollow_user' => 'relationships#unfollow_user'
 
-  get '/comments/get_all_comments' => 'comments#get_all_comments'
+  # Artwork show page
+  get '/comments/all_comments' => 'comments#all_comments'
   post '/comments/new_comment' => 'comments#new_comment'
-
-  get '/likes/get_all_likes' => 'likes#get_all_likes'
   post '/likes/like_artwork' => 'likes#like_artwork'
   post '/likes/unlike_artwork' => 'likes#unlike_artwork'
-
+  
 end
