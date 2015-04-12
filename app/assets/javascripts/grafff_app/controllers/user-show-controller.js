a3a8fc752ffe21f95cebf4badaa176ebfb6aea69
Mathew Hawley <mathew@mathewhawley.com>
2015-04-12 14:08:01 +0100

    Add file upload functionality to user-show-controller.js

--- app/assets/javascripts/grafff_app/controllers/user-show-controller.js
+++ app/assets/javascripts/grafff_app/controllers/user-show-controller.js
@@ -1,4 +1,4 @@
-app.controller('UserShowController', ['$scope', '$http', '$routeParams', 'UserFactory', 'RelationshipFactory', function($scope, $http, $routeParams, UserFactory, RelationshipFactory) {
+app.controller('UserShowController', ['$scope', '$http', '$routeParams', '$route', 'UserFactory', 'RelationshipFactory', 'ArtworkFactory', function($scope, $http, $routeParams, $route, UserFactory, RelationshipFactory, ArtworkFactory) {
 
   // Function to check if current user already likes 'this' artwork – called on page load
   function checkIfFollowing(currentUserId, followedId) {
@@ -79,6 +79,70 @@ app.controller('UserShowController', ['$scope', '$http', '$routeParams', 'UserFa
       // Set variable to false – this will switch 'unfollow' button to 'follow'
       $scope.followingThisArtist = false;
     });
+  };
+
+  // NEW ARTWORK FORM
+  // Set new artwork location
+  $scope.getPosition = function(position) {
+
+    longitude = position.latLng.D;
+    latitude = position.latLng.k;
+
+    $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true').then(function(response) {
+      $scope.nearestAddress = response.data.results[0].formatted_address;
+    });
+
+  };
+
+  // File uploads
+  $scope.creds = {
+    bucket: 'grafff',
+    access_key: gon.aws_access_key,
+    secret_key: gon.aws_secret_key
+  };
+   
+  $scope.upload = function(address) {
+    // Configure The S3 Object 
+    AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+    AWS.config.region = 'eu-west-1';
+    var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
+   
+    if($scope.file) {
+      var params = { Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
+   
+      bucket.putObject(params, function(err, data) {
+        if(err) {
+          // There Was An Error With Your S3 Config
+          console.log(err)
+          alert(err.message);
+          return false;
+        } else {
+          // Success!
+          // alert('Upload Done');
+        };
+      })
+      .on('httpUploadProgress',function(progress) {
+            // Log Progress Information
+            console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+          });
+    } else {
+      // No File Selected
+      alert('No File Selected');
+    };
+
+    $scope.imageUrl = 'https://s3-eu-west-1.amazonaws.com/grafff/' + $scope.file.name;
+
+    $scope.newArtwork['address'] = address;
+    $scope.newArtwork['lat'] = latitude;
+    $scope.newArtwork['lng'] = longitude;
+    $scope.newArtwork['user_id'] = $scope.user.id;
+    $scope.newArtwork['image'] = $scope.imageUrl;
+
+    ArtworkFactory.addArtwork($scope.newArtwork).then(function(response) {
+      $scope.artistArtworks.push(response.data);
+      $route.reload();
+    });
+
   };
 
 }]);
