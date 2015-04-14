0f6ef1240b3448d708a4645858347d2e67dfcc8c
Mathew Hawley <mathew@mathewhawley.com>
2015-04-14 19:18:59 +0100

    Split out main map to it's own page

--- app/assets/javascripts/grafff_app/controllers/index-controller.js
+++ app/assets/javascripts/grafff_app/controllers/index-controller.js
@@ -9,7 +9,8 @@ app.controller('IndexController', [
     $http,
     $timeout,
     UserFactory,
-    ArtworkFactory) {
+    ArtworkFactory
+  ) {
 
   // Get all artists
   UserFactory.getPopularArtists()
@@ -17,59 +18,10 @@ app.controller('IndexController', [
     $scope.popularArtists = response.data;
   });
 
-  // Get all artworks for main map
-  ArtworkFactory.getMainMapArtwork()
-  .then(function(response) {
-    $scope.mapArtwork = [];
-    var iterator = 0;
-    for (var i = 0; i < response.data.length; i++) {
-      $timeout(function() {
-        $scope.mapArtwork.push(response.data[iterator++]);
-      }, i * 100);
-    }
-  });
-
   // Get latest 3 artworks
   ArtworkFactory.getLatestArtworks()
   .then(function(response) {
     $scope.latestArtworks = response.data;
   });
-
-  // DATE PICKER
-  // ===========
-
-  $scope.today = function() {
-    $scope.dt = null;
-  };
-  $scope.today();
-
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
-
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
-
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
-
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-
-    $scope.opened = true;
-  };
-
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
-
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
 
 }]);
---
+++ app/assets/javascripts/grafff_app/controllers/main-map-controller.js
@@ -0,0 +1,60 @@
+app.controller('MainMapController', [
+  '$scope',
+  '$timeout',
+  'ArtworkFactory',
+  function(
+    $scope,
+    $timeout,
+    ArtworkFactory
+  ) {
+
+  // Get all artworks for main map
+  ArtworkFactory.getMainMapArtwork()
+  .then(function(response) {
+    $scope.mapArtwork = [];
+    var iterator = 0;
+    for (var i = 0; i < response.data.length; i++) {
+      $timeout(function() {
+        $scope.mapArtwork.push(response.data[iterator++]);
+      }, i * 100);
+    }
+  });
+
+  // DATE PICKER
+  // ===========
+
+  $scope.today = function() {
+    $scope.dt = null;
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+}]);
