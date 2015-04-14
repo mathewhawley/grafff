465202cce688b83b63adbccf2206d468f8b2ac88
Mathew Hawley <mathew@mathewhawley.com>
2015-04-14 10:55:18 +0100

    Create search factory action for datepicker filter

--- app/assets/javascripts/grafff_app/services/search-factory.js
+++ app/assets/javascripts/grafff_app/services/search-factory.js
@@ -3,6 +3,9 @@ app.factory('SearchFactory', ['$http', function($http) {
   var SearchFactory = {
     searchUsers: function(query) {
       return $http.post('/users/search', query);
+    },
+    filterArtwork: function(date) {
+      return $http.post('/artworks/filter_by_date', date);
     }
   };
 
