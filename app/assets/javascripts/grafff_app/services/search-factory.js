4bb5e17ce0ba3f2ba1a0cc1e20e5a8cc065ddf0b
Mathew Hawley <mathew@mathewhawley.com>
2015-04-16 10:46:23 +0100

    Tweak map height, datepicker formula

--- app/assets/javascripts/grafff_app/services/search-factory.js
+++ app/assets/javascripts/grafff_app/services/search-factory.js
@@ -6,7 +6,7 @@ app.factory('SearchFactory', ['$http', function($http) {
     },
     filterArtwork: function(date) {
       console.log(date);
-      unixtime = Date.parse(date) / 1000;
+      unixtime = Date.parse(date) / 1000 + 9000;
       console.log(unixtime);
       return $http.post('/artworks/filter_by_date', unixtime);
     }
